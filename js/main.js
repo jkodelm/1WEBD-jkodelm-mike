9e3794ef42022ea5d52e83a961ae8a3b61bf6b84
jklm <kylianjlm@gmail.com>
2025-01-12 14:41:12 +0100

    spare me

--- js/main.js
+++ js/main.js
@@ -1,22 +1,16 @@
-/* TODO :
-- Setup carousel elements from class
-- More repsonsive
-- Tags top: Dernièrement sorti, Votre cinéma, News
-- Rename
-*/
-
 class heroCarousel {
     constructor() {
-        this.cards = [...document.querySelector("#hero-content").children]; //javascript... we need .arrayfy()...
+        this.cards = [...document.querySelector("#hero-content").children];
         this.pauseBtn = document.querySelector("#hero-pause")
-        this.indicators = [...document.querySelector("#hero-indicators").children]
+        this.indicators = [...document.querySelector("#hero-indicators").children];
 
         this.cardClasses = ["hero-farleft", "hero-left", "hero-main", "hero-right", "hero-farright"];
-        this.nbCards = this.cards.length
+        this.nbCards = this.cards.length;
         this.index = this.cards.length - 1;
-        this.stopped = false
-        this.indicatorAnimation = null
-        this.slide('r')
+        this.stopped = false;
+        this.sliding = false;
+
+        this.slide('r');
 
         const style = window.getComputedStyle(document.body)
         this.slidingTime = style.getPropertyValue('--timeHeroAnimation').slice(0, -2);
@@ -31,105 +25,128 @@ class heroCarousel {
                 this.goToSlide(i);
             });
         });
-        this.currentIndicator = this.indicators[this.index]
+        this.currentIndicator = this.indicators[this.index];
     }
-  
-    slide(direction, newIndex=null) {
 
+    slide(direction='r', newIndex = null) {
         this.sliding = true;
-        this.manageIndicator('stop')
-
-        if (direction === 'r') {
-            this.index = (this.index + 1) % this.nbCards
+        this.manageIndicator('stop');
+        
+        if (newIndex !== null) {
+            this.index = newIndex;
+            this.manageIndicator('change');
+        } else if (direction === 'r') {
+            this.index = (this.index + 1) % this.nbCards;
         } else if (direction === 'l') {
-            this.index = this.correctIndex(this.index - 1)
-        } else {
-            this.index = newIndex
-        }
+            this.index = this.correctIndex(this.index - 1);
+        };
 
         this.cards.forEach((card, ind) => {
-            card.zIndex = this.indexValue(ind, this.index, this.nbCards) // fix
-            card.classList = 'hero-card ' + this.cardClasses[(this.correctIndex(ind - this.index) + 2) % this.nbCards] //i love math
+            card.zIndex = this.indexValue(ind);
+            card.classList = 'hero-card ' + this.cardClasses[(this.correctIndex(ind - this.index) + 2) % this.nbCards];
         });
 
-        setTimeout(() => {
-            this.sliding = false;
-            this.startAutoSlide();
-        }, this.slidingTime);
+        if (newIndex === null) {
+            setTimeout(() => {
+                this.sliding = false;
+                this.startAutoSlide();
+            }, this.slidingTime);
+        };
 
-        if (direction !== '') {
-            this.manageIndicator('span')
+        if (this.stopped) {
+            this.manageIndicator('change');
         } else {
-            this.manageIndicator('change')
+            this.manageIndicator('span');
         }
-    }
+    };
 
     correctIndex(index) {
-        return (index + this.nbCards) % this.nbCards
-    }
-  
-    indexValue(i, ind, maxi) {
-        let val = i - ind
-        if (val < 0) { return maxi - (maxi + val) }
-        return maxi - val
-    }
+        return (index + this.nbCards) % this.nbCards;
+    };
+
+    indexValue(i) {
+        let val = i - this.index;
+        let maxi = this.nbCards;
+        if (val < 0) { return maxi - (maxi + val); }
+        return maxi - val;
+    };
 
     togglePause() {
         this.stopped = !this.stopped;
-        this.pauseBtn.classList.toggle('i-play', this.stopped)
+        this.pauseBtn.classList.toggle('i-play', this.stopped);
         this.pauseBtn.classList.toggle('i-pause', !this.stopped);
 
         if (this.stopped) {
-            this.stopAutoSlide()
+            this.stopAutoSlide();
+            this.manageIndicator('pause');
         } else {
-            this.startAutoSlide()
+            this.manageIndicator('resume');
+            this.startAutoSlide();
         }
     }
 
     startAutoSlide() {
-        if (this.stopped) { return }
+        if (this.stopped) return;
         this.stopAutoSlide();
 
         this.autoSlideInterval = setInterval(() => {
             if (!this.sliding) {
-                this.manageIndicator('stop', this.correctIndex(this.index - 1))
+                this.manageIndicator('stop', this.correctIndex(this.index - 1));
                 this.slide('r');
-                this.manageIndicator('span')
+                this.manageIndicator('span');
             }
         }, this.autoSlideTime);
     }
 
     stopAutoSlide() {
         clearInterval(this.autoSlideInterval);
-        this.manageIndicator('pause')
+        this.manageIndicator('pause');
     }
 
-    manageIndicator(action, index=this.index) {
+    manageIndicator(action, index = this.index) {
         const indic = this.indicators[index];
-        if (action === 'span') { 
+        const timeIndic = indic.firstChild;
+
+        if (action === 'span') {
+            indic.classList.add('hero-indicator-moving');
             indic.style.width = '3rem';
-            indic.firstChild.style.width = '100%';
-        } else if (action === 'change') { 
-            indic.style.backgroundColor = '#6A6A6A';
+            timeIndic.style.animation = 'moveIndicator var(--timeAutoSlide) linear forwards';
+            timeIndic.style.animationPlayState = 'running';
+            timeIndic.onanimationend = () => {
+                if (!this.stopped) {
+                    indic.style.width = '1rem';
+                    this.slide('r');
+                }
+            };
+        } else if (action === 'pause') {
+            if (timeIndic.style.animation) {
+                timeIndic.style.animationPlayState = 'paused';
+            }
+        } else if (action === 'resume') {
+            if (timeIndic.style.animation) {
+                timeIndic.style.animationPlayState = 'running';
+            }
+            indic.style.width = '3rem';
+            indic.style.backgroundColor = "var(--dark)";
         } else if (action === 'stop') {
-            indic.style.width = '1rem';
-            indic.style.backgroundColor = '#242424';
-            indic.firstChild.classList.add('notransition')
-            indic.firstChild.style.width = '0';
-            window.setTimeout(() => { indic.firstChild.classList.remove('notransition') }, 50, indic)
-        } else if (action === 'pause') { 
-            
+            indic.style.backgroundColor = "var(--dark)";
+            indic.style.width = '1rem'
+            timeIndic.style.animation = 'none';
+        } else if (action === 'change') {
+            indic.style.backgroundColor = "var(--gray)";
         }
     }
 
     goToSlide(index) {
-        this.manageIndicator('change', index)
-        this.slide('', index)
-        this.stopAutoSlide()
+        if (!this.stopped) { this.togglePause() }
+        this.manageIndicator('change', index);
+        this.slide('', index);
+        this.stopAutoSlide();
     }
 }
 
 function main() {
     const heroCarousel1 = new heroCarousel();
 }
+
 window.onload = main;

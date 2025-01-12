d353b0e002bbe532b6ec5408734444ecce6a7e38
jklm <kylianjlm@gmail.com>
2025-01-12 10:15:46 +0100

    Indicators click and slide. Need pause and stopAutoSlide

--- js/main.js
+++ js/main.js
@@ -5,164 +5,23 @@
 - Rename
 */
 
-/* PREVIOUS
-
-class Carousel {
-    constructor(masterName, cardName, visibleSlides, attributes, leftBtn, rightBtn, pauseBtnName = null, masterIndicatorsName = null) {
-        this.parent = document.querySelector(`#${masterName}`);
-
-        this.slides = Array.from(this.parent.querySelectorAll(`.${cardName}`));
-
-        this.nbSlides = this.slides.length;
-        this.middleNbSlides = Math.floor(this.nbSlides / 2);
-        this.onScreenSlides = visibleSlides;
-        this.arrayAttributes = attributes;
-        this.index = 0;
-        this.stopped = false;
-
-        var style = window.getComputedStyle(document.body)
-        this.slidingTime = style.getPropertyValue('--timeHeroAnimation').slice(0, -2);
-        this.autoSlideTime = style.getPropertyValue('--timeAutoSlide').slice(0, -2);
-        console.log(this.slidingTime, this.autoSlideTime)
-
-        this.leftBtn = document.querySelector(`#${leftBtn}`)
-        this.leftBtn.addEventListener('click', () => { this.slide('left') })
-        this.rightBtn = document.querySelector(`#${rightBtn}`)
-        this.rightBtn.addEventListener('click', () => { this.slide('right') })
-
-        if (pauseBtnName) {
-            this.pauseBtn = document.querySelector(`#${pauseBtnName}`);
-            this.pauseBtn.addEventListener('click', () => { this.togglePause(); });
-        }
-
-        if (masterIndicatorsName) {
-            this.indicators = Array.from(document.querySelector(`#${masterIndicatorsName}`).children);
-            this.indicators.forEach((indicator, i) => {
-                indicator.addEventListener('click', () => {
-                    this.goToSlide(i);
-                });
-            });
-            this.currentIndicator = this.indicators[this.index]
-        }
-
-        this.switchIndicator()
-        this.startAutoSlide();
-    }
-
-    startAutoSlide() {
-        if (this.stopped) { return }
-        this.stopAutoSlide();
-
-        this.autoSlideInterval = setInterval(() => {
-            if (!this.sliding) {
-                this.manageIndicator(this.indicators[this.index], 'stop')
-                this.slide('right');
-                this.manageIndicator(this.indicators[this.index], 'span')
-            }
-        }, this.autoSlideTime);
-    }
-
-    stopAutoSlide() {
-        clearInterval(this.autoSlideInterval);
-        this.manageIndicator(this.indicators[this.index], 'pause')
-    }
-
-    manageIndicator(indic, action) {
-        if (action === 'instant') {
-            indic.style.backgroundColor = 'rgba(255, 255, 255, 0.3)';
-        } else if (action === 'span') {
-            indic.firstChild.style.width = '100%';
-        } else if (action === 'stop') {
-            indic.firstChild.style.transition = 'none';
-            indic.firstChild.style.width = '0';
-            setTimeout(() => {
-                indic.firstChild.style.transition = `width ${this.autoSlideTime}ms`;
-            }, 0);
-        } else if (action === 'pause') {
-            indic.firstChild.style.transition = 'none';
-        }
-    }
-    
-
-    slide(direction) {
-        if (this.sliding) { return; }
-    
-        this.sliding = true;
-        this.index = this.correctIndex(this.index + (direction === 'left' ? -1 : 1));
-        console.log('ccur ind', this.index)
-
-        for(let i=this.index; i < this.correctIndex(this.index + 4); this.correctIndex(i++)) {
-            console.log('tyèft',i)
-            let slide = this.slides[i]
-            slide.className = `hero-card ${this.arrayAttributes[i]}`;
-            slide.style.zIndex = this.nbSlides - this.correctIndex(i - this.middleNbSlides);
-        }
-        
-        /* this.slides.forEach((slide, i) => {
-            const attributeIndex = this.correctIndex(i - this.index);
-            slide.className = `hero-card ${this.arrayAttributes[attributeIndex]}`;
-            slide.style.zIndex = this.nbSlides - this.correctIndex(attributeIndex - this.middleNbSlides);
-        }); 
-
-        setTimeout(() => {
-            this.sliding = false;
-            this.startAutoSlide();
-        }, this.slidingTime);
-
-        this.switchIndicator();
-    }
-
-    correctIndex(index) {
-        return (index + this.nbSlides) % this.nbSlides;
-    }
-
-    togglePause() {
-        this.stopped = !this.stopped;
-        this.pauseBtn.classList.toggle('i-play', this.stopped)
-        this.pauseBtn.classList.toggle('i-pause', !this.stopped);
-
-        if (this.stopped) {
-            console.log('curr pause')
-            this.stopAutoSlide()
-        } else {
-            this.startAutoSlide()
-        }
-    }
-
-    switchIndicator() {
-        if (!this.indicators) { return };
-
-        this.indicators.forEach((indic) => {
-            indic.classList = 'indicator'
-        });
-        let currIndic = this.indicators[this.index]
-        currIndic.classList = 'indicator indicator-active'
-    }
-
-    goToSlide(targetIndex) {
-        if (targetIndex === this.index) { return; }
-        const direction = targetIndex > this.index ? 'right' : 'left';
-        while (this.index !== targetIndex) {
-            this.slide(direction);
-        }
-    }
-}*/
-
 class heroCarousel {
     constructor() {
-        this.cards = document.querySelectorAll(".hero-card");
+        this.cards = [...document.querySelector("#hero-content").children]; //javascript... we need .arrayfy()...
         this.pauseBtn = document.querySelector("#hero-pause")
-        this.indicators = document.querySelectorAll(".hero-indicators")
+        this.indicators = [...document.querySelector("#hero-indicators").children]
 
         this.cardClasses = ["hero-farleft", "hero-left", "hero-main", "hero-right", "hero-farright"];
         this.nbCards = this.cards.length
         this.index = this.cards.length - 1;
         this.stopped = false
-        this.slide()
+        this.indicatorAnimation = null
+        this.slide('r')
 
-        var style = window.getComputedStyle(document.body)
+        const style = window.getComputedStyle(document.body)
         this.slidingTime = style.getPropertyValue('--timeHeroAnimation').slice(0, -2);
         this.autoSlideTime = style.getPropertyValue('--timeAutoSlide').slice(0, -2);
+        this.startAutoSlide()
 
         document.querySelector("#hero-arrow-left").addEventListener("click", () => { this.slide('l') });
         document.querySelector("#hero-arrow-right").addEventListener("click", () => { this.slide('r') });
@@ -176,18 +35,37 @@ class heroCarousel {
     }
   
     slide(direction, newIndex=null) {
+
+        this.sliding = true;
+        this.manageIndicator('stop')
+
         if (direction === 'r') {
             this.index = (this.index + 1) % this.nbCards
         } else if (direction === 'l') {
-            this.index = (this.index - 1 + this.nbCards) % this.nbCards
+            this.index = this.correctIndex(this.index - 1)
         } else {
             this.index = newIndex
         }
 
         this.cards.forEach((card, ind) => {
-            card.zIndex = this.indexValue(ind, this.index, this.nbCards)
-            card.classList = 'hero-card ' + this.cardClasses[((ind - this.index + this.nbCards) % this.nbCards + 2) % this.nbCards] //i love math
+            card.zIndex = this.indexValue(ind, this.index, this.nbCards) // fix
+            card.classList = 'hero-card ' + this.cardClasses[(this.correctIndex(ind - this.index) + 2) % this.nbCards] //i love math
         });
+
+        setTimeout(() => {
+            this.sliding = false;
+            this.startAutoSlide();
+        }, this.slidingTime);
+
+        if (direction !== '') {
+            this.manageIndicator('span')
+        } else {
+            this.manageIndicator('change')
+        }
+    }
+
+    correctIndex(index) {
+        return (index + this.nbCards) % this.nbCards
     }
   
     indexValue(i, ind, maxi) {
@@ -202,7 +80,6 @@ class heroCarousel {
         this.pauseBtn.classList.toggle('i-pause', !this.stopped);
 
         if (this.stopped) {
-            console.log('curr pause')
             this.stopAutoSlide()
         } else {
             this.startAutoSlide()
@@ -215,22 +92,40 @@ class heroCarousel {
 
         this.autoSlideInterval = setInterval(() => {
             if (!this.sliding) {
-                this.manageIndicator(this.indicators[this.index], 'stop')
+                this.manageIndicator('stop', this.correctIndex(this.index - 1))
                 this.slide('r');
-                this.manageIndicator(this.indicators[this.index], 'span')
+                this.manageIndicator('span')
             }
         }, this.autoSlideTime);
     }
 
     stopAutoSlide() {
         clearInterval(this.autoSlideInterval);
-        this.manageIndicator(this.indicators[this.index], 'pause')
+        this.manageIndicator('pause')
     }
 
-    manageIndicator() {}
+    manageIndicator(action, index=this.index) {
+        const indic = this.indicators[index];
+        if (action === 'span') { 
+            indic.style.width = '3rem';
+            indic.firstChild.style.width = '100%';
+        } else if (action === 'change') { 
+            indic.style.backgroundColor = '#6A6A6A';
+        } else if (action === 'stop') {
+            indic.style.width = '1rem';
+            indic.style.backgroundColor = '#242424';
+            indic.firstChild.classList.add('notransition')
+            indic.firstChild.style.width = '0';
+            window.setTimeout(() => { indic.firstChild.classList.remove('notransition') }, 50, indic)
+        } else if (action === 'pause') { 
+            
+        }
+    }
 
     goToSlide(index) {
-        this.slide(newIndex=index)
+        this.manageIndicator('change', index)
+        this.slide('', index)
+        this.stopAutoSlide()
     }
 }
 
@@ -238,24 +133,3 @@ function main() {
     const heroCarousel1 = new heroCarousel();
 }
 window.onload = main;
-
-/* 
-let a = this.correctIndex(this.index - 2)
-this.slides[a].classList.add(this.arrayAttributes[1])
-this.slides[a].classList.remove('hidden')
-
-a = this.correctIndex(this.index - 1)
-this.slides[a].classList.add(this.arrayAttributes[2])
-this.slides[a].classList.remove(this.arrayAttributes[1])
-
-this.slides[this.index].classList.add(this.arrayAttributes[3])
-this.slides[this.index].classList.remove(this.arrayAttributes[2])
-
-a = this.correctIndex(this.index + 1);
-this.slides[a].classList.add(this.arrayAttributes[4]);
-this.slides[a].classList.remove(this.arrayAttributes[3]);
-
-window.setTimeout((ind = a) => {
-    this.slides[ind].classList.remove(this.arrayAttributes[4]);
-    this.slides[ind].classList.add('hidden');
-}, 1000);*/

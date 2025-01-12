1649b66e0144c7370532e09dc3dd220ffc83e6a0
jklm <kylianjlm@gmail.com>
2025-01-12 18:34:23 +0100

    ALMOST DONE OMG

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+/* TO DO:
+? When click on indicator, doesn't widen + becomes gray
+- Clearer func names?
+- Fix zIndexs
+- I can spam now?
+- Keep anim values FROM CSS or enter directly in JS?
+- Apply JS styling guide
+
+Better responsive height*/
+
 class heroCarousel {
     constructor() {
-        this.cards = [...document.querySelector("#hero-content").children];
-        this.pauseBtn = document.querySelector("#hero-pause")
+        this.cards = [...document.querySelector("#hero-content").children]; //javascript... we need .arrayfy()...
+        this.pauseBtn = document.querySelector("#hero-pause");
         this.indicators = [...document.querySelector("#hero-indicators").children];
 
         this.cardClasses = ["hero-farleft", "hero-left", "hero-main", "hero-right", "hero-farright"];
@@ -10,143 +20,110 @@ class heroCarousel {
         this.stopped = false;
         this.sliding = false;
 
-        this.slide('r');
-
-        const style = window.getComputedStyle(document.body)
+        // TEMP FOR TESTS
+        const style = window.getComputedStyle(document.body);
         this.slidingTime = style.getPropertyValue('--timeHeroAnimation').slice(0, -2);
         this.autoSlideTime = style.getPropertyValue('--timeAutoSlide').slice(0, -2);
-        this.startAutoSlide()
 
         document.querySelector("#hero-arrow-left").addEventListener("click", () => { this.slide('l') });
         document.querySelector("#hero-arrow-right").addEventListener("click", () => { this.slide('r') });
         this.pauseBtn.addEventListener("click", () => { this.togglePause() });
         this.indicators.forEach((indicator, i) => {
-            indicator.addEventListener('click', () => {
-                this.goToSlide(i);
-            });
+            indicator.addEventListener('click', () => { this.goToSlide(i) });
         });
-        this.currentIndicator = this.indicators[this.index];
-    }
 
-    slide(direction='r', newIndex = null) {
-        this.sliding = true;
-        this.manageIndicator('stop');
-        
+        this.slide('r');
+        this.startAutoSlide();
+    };
+
+    slide(direction='r', newIndex=null) {
+        if (this.sliding) { return };
+        //this.sliding = true;
+
         if (newIndex !== null) {
             this.index = newIndex;
-            this.manageIndicator('change');
-        } else if (direction === 'r') {
-            this.index = (this.index + 1) % this.nbCards;
-        } else if (direction === 'l') {
-            this.index = this.correctIndex(this.index - 1);
+        } else {
+            this.index = this.indexValue(direction);
         };
 
-        this.cards.forEach((card, ind) => {
-            card.zIndex = this.indexValue(ind);
-            card.classList = 'hero-card ' + this.cardClasses[(this.correctIndex(ind - this.index) + 2) % this.nbCards];
+        this.cards.forEach((card, i) => {
+            i = (i - 3 + this.nbCards) % this.nbCards
+            //card.innerHTML = this.getZIndex(i);
+            card.className = `hero-card ${this.cardClasses[(i - this.index + this.nbCards) % this.nbCards]}`;
         });
 
-        if (newIndex === null) {
-            setTimeout(() => {
-                this.sliding = false;
-                this.startAutoSlide();
-            }, this.slidingTime);
-        };
+        this.manageIndicators();
 
-        if (this.stopped) {
-            this.manageIndicator('change');
-        } else {
-            this.manageIndicator('span');
-        }
+        setTimeout(() => {
+            this.sliding = false;
+            if (!this.stopped) { this.startAutoSlide() };
+        }, this.slidingTime);
     };
 
-    correctIndex(index) {
-        return (index + this.nbCards) % this.nbCards;
+    indexValue(direction) {
+        return direction === 'r' ? (this.index + 1) % this.nbCards : (this.index - 1 + this.nbCards) % this.nbCards;
     };
 
-    indexValue(i) {
-        let val = i - this.index;
-        let maxi = this.nbCards;
-        if (val < 0) { return maxi - (maxi + val); }
-        return maxi - val;
+    goToSlide(index) {
+        if (!this.stopped) { this.togglePause() };
+        this.slide('', index);
     };
+
+    /* getZIndex(i) {
+        return (this.nbCards - Math.abs((i - this.index + this.nbCards) % this.nbCards) + 1) % this.nbCards;
+    } */
 
     togglePause() {
         this.stopped = !this.stopped;
         this.pauseBtn.classList.toggle('i-play', this.stopped);
         this.pauseBtn.classList.toggle('i-pause', !this.stopped);
 
+        this.indicators.forEach((indicator) => {
+            const timeBar = indicator.firstElementChild;
+            timeBar.style.animationPlayState = this.stopped ? 'paused' : 'running';
+        });
+
         if (this.stopped) {
             this.stopAutoSlide();
-            this.manageIndicator('pause');
         } else {
-            this.manageIndicator('resume');
             this.startAutoSlide();
-        }
-    }
+        };
+    };
 
     startAutoSlide() {
-        if (this.stopped) return;
+        if (this.stopped) { return };
         this.stopAutoSlide();
 
-        this.autoSlideInterval = setInterval(() => {
-            if (!this.sliding) {
-                this.manageIndicator('stop', this.correctIndex(this.index - 1));
+        this.indicators[this.index].firstChild.onanimationend = () => {
+            if (!this.stopped) {
                 this.slide('r');
-                this.manageIndicator('span');
             }
-        }, this.autoSlideTime);
-    }
+        };
+    };
 
     stopAutoSlide() {
         clearInterval(this.autoSlideInterval);
-        this.manageIndicator('pause');
-    }
+    };
 
-    manageIndicator(action, index = this.index) {
-        const indic = this.indicators[index];
-        const timeIndic = indic.firstChild;
+    manageIndicators() { /*origin*/
+        this.indicators.forEach((indicator, i) => {
+            const timeBar = indicator.firstElementChild;
 
-        if (action === 'span') {
-            indic.classList.add('hero-indicator-moving');
-            indic.style.width = '3rem';
-            timeIndic.style.animation = 'moveIndicator var(--timeAutoSlide) linear forwards';
-            timeIndic.style.animationPlayState = 'running';
-            timeIndic.onanimationend = () => {
-                if (!this.stopped) {
-                    indic.style.width = '1rem';
-                    this.slide('r');
-                }
+            if (i === this.index) {
+                //indicator.style.backgroundColor = 'var(--gray)';
+                indicator.style.width = '3rem';
+                timeBar.style.animation = `moveIndicator ${this.autoSlideTime}ms linear forwards`;
+                timeBar.style.animationPlayState = this.stopped ? 'paused' : 'running';
+            } else {
+                //indicator.style.backgroundColor = 'var(--dark)';
+                indicator.style.width = '1rem';
+                timeBar.style.animation = 'none';
             };
-        } else if (action === 'pause') {
-            if (timeIndic.style.animation) {
-                timeIndic.style.animationPlayState = 'paused';
-            }
-        } else if (action === 'resume') {
-            if (timeIndic.style.animation) {
-                timeIndic.style.animationPlayState = 'running';
-            }
-            indic.style.width = '3rem';
-            indic.style.backgroundColor = "var(--dark)";
-        } else if (action === 'stop') {
-            indic.style.backgroundColor = "var(--dark)";
-            indic.style.width = '1rem'
-            timeIndic.style.animation = 'none';
-        } else if (action === 'change') {
-            indic.style.backgroundColor = "var(--gray)";
-        }
-    }
-
-    goToSlide(index) {
-        if (!this.stopped) { this.togglePause() }
-        this.manageIndicator('change', index);
-        this.slide('', index);
-        this.stopAutoSlide();
-    }
-}
+        });
+    };
+};
 
 function main() {
-    const heroCarousel1 = new heroCarousel();
+    new heroCarousel();
 }
-
 window.onload = main;

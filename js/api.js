94f09f50b3c6c34b228478b996be84b5784ba116
miked06 <mike.dewitte@supinfo.com>
2025-01-13 15:09:12 +0100

    start movie card

--- js/api.js
+++ js/api.js
@@ -1,38 +1,26 @@
-const apiKey = 'c6e5cf6d';
-const movieTitle = 'batman';
+/* const apiKey = 'c6e5cf6d';
+const movieTitle = 'batman'
 const apiUrl = `https://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`;
+const movie = fetch(apiUrl);
+
 
 fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        if (data.Response === 'True') {
-            const movieName = data.Title;
-            const posterUrl = data.Poster;
-            const moviePlot = data.Plot;
-            const movieGenre = data.Genre;
-            const movieActor = data.Actors;
-            const movieRatings = data.Ratings; 
-            const moviedvd = data.DVD;
+.then(response => console.log(response.status) || response) // output the status and return response
+  .then(response => response.text()) // send response body to next then chain
+  .then(body => console.log(body)) // you can use response body here*/
 
-            let ratingsHTML = '';
-            movieRatings.forEach(rating => {
-                ratingsHTML += `<p><strong>${rating.Source}</strong>: ${rating.Value}</p>`;
-            });
+const movie = {"Title":"Batman","Year":"1989","Rated":"PG-13","Released":"23 Jun 1989","Runtime":"126 min","Genre":"Action, Adventure","Director":"Tim Burton","Writer":"Bob Kane, Sam Hamm, Warren Skaaren","Actors":"Michael Keaton, Jack Nicholson, Kim Basinger","Plot":"The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker.","Language":"English, French, Spanish","Country":"United States, United Kingdom","Awards":"Won 1 Oscar. 11 wins & 28 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BYzZmZWViM2EtNzhlMi00NzBlLWE0MWEtZDFjMjk3YjIyNTBhXkEyXkFqcGc@._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.5/10"},{"Source":"Rotten Tomatoes","Value":"77%"},{"Source":"Metacritic","Value":"69/100"}],"Metascore":"69","imdbRating":"7.5","imdbVotes":"414,807","imdbID":"tt0096895","Type":"movie","DVD":"N/A","BoxOffice":"$251,409,241","Production":"N/A","Website":"N/A","Response":"True"}
+const movieTitle = movie.Title;
+console.log(movieTitle);
+const movieposter = movie.Poster;
+console.log(movieposter);
+const movieplot = movie.Plot;
+console.log(movieplot);
+const moviegenre = movie.Genre;
+console.log(moviegenre);
+const movieactors = movie.Actors;
+console.log(movieactors);
+const movieratings = movie.Ratings;
+console.log(movieratings);
 
-            const movieInfoDiv = document.getElementById('movie-info');
-            movieInfoDiv.innerHTML = `
-                <h2>${movieName}</h2>
-                <img src="${posterUrl}" alt="${movieName} Poster">
-                <p><strong>Plot:</strong> ${moviePlot}</p>
-                <p><strong>Genre:</strong> ${movieGenre}</p>
-                <p><strong>Actors:</strong> ${movieActor}</p>
-                <div><strong>Ratings:</strong> ${ratingsHTML}</div>
-                <p><strong>DVD:</strong> ${moviedvd}</p> 
-            `;
-        } else {
-            document.getElementById('movie-info').innerHTML = `<p>Movie not found!</p>`;
-        }
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+//poster, nom , lien en savoir plus, bonus : petit resume du film , 

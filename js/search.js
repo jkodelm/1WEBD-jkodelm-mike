2f57ae461ff2d73bb0f19983081f82f47f85a552
jklm <kylianjlm@gmail.com>
2025-01-14 21:41:31 +0100

    Search bar phone avancé

--- js/search.js
+++ js/search.js
@@ -4,48 +4,50 @@ padding
 Icon load faster w/ button?*/
 
 let navSearch, navSearchInput, navSearchBtn
-const searchDeployed = false
+let searchDeployed = false
+
+function search() {
+    if (!searchDeployed && window.innerWidth < 480) {
+        navSearchBtn.removeEventListener('click', search);
+        deploySearch()
+    } else {
+        console.log('recherche',navSearchInput.value,'sur google')
+    }
+    
+}
 
 function deploySearch () {
-    navSearch.classList.add(nav-search-deployed)
+    searchDeployed = true
+    navSearch.classList.add('nav-search-deployed')
 
     let backBtn = document.createElement('div');
     backBtn.classList.add('icon');
     backBtn.classList.add('i-left');
     backBtn.addEventListener('click', () => {
         backBtn.remove()
-        undeploySearch(navSearch, navSearchInput, backBtn)
+        undeploySearch()
     })
     navSearchInput.insertAdjacentElement('beforebegin', backBtn)
-    navSearchInput.style.flex = '1'
     navSearchInput.focus();
+    navSearchBtn.addEventListener('click', search)
 }
 
 function undeploySearch () {
-    navSearch.style.width = 'auto'
-    navSearch.style.backgroundColor = 'inherit';
-    navSearch.style.position = 'block';
+    searchDeployed = false
+    
+    navSearch.classList.remove('nav-search-deployed')
 
-    navSearchInput.style.flex = 'none'
-    navSearchInput.style.display = 'initial';
-
-    navSearch.style.cssText = ""
-    navSearchInput.style.cssText = ""
+    //navSearch.style.cssText = ""
+    //navSearchInput.style.cssText = ""
 }
 
 function main() {
     navSearch = document.querySelector('.nav-search')
     navSearchInput = document.querySelector('#nav-search-input')
     navSearchBtn = document.querySelector('#nav-search-button')
-    navSearchBtn.addEventListener('click', () => {
-        navSearchBtn.removeEventListener('click', () => {})
-        deploySearch()
-    })
-    window.onresize = () => {
-        if (searchDeployed && window.innerWidth > 480) {
-            undeploySearch()
-        }
-    }
+    navSearchBtn.addEventListener('click', search)
+    window.onresize = () => { if (searchDeployed && window.innerWidth > 480) { undeploySearch() } }
+    
 }
 
 window.onload = main

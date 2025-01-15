e892ed537312bffad7a18e10682e0f7c236129f2
jklm <kylianjlm@gmail.com>
2025-01-15 12:15:19 +0100

    Fixed search bar

--- js/search.js
+++ js/search.js
@@ -1,53 +1,39 @@
-/* Si click enter avec search bar on focus, SEARCH,
-AJOUTER SEARCH DANS URL, si dans url quand load, affichier, sinon quand back vec téléphone, enlève search.
-padding
-Icon load faster w/ button?
-overlay function*/
-
-let navSearch, navSearchInput, navSearchBtn
-let searchDeployed = false
-console.log('loaded')
-
-function search() {
-    if (!searchDeployed && window.innerWidth < 480) {
-        navSearchBtn.removeEventListener('click', search);
-        deploySearch()
-    } else {
-        console.log('recherche',navSearchInput.value,'sur google')
-    } 
-}
-
-function deploySearch () {
-    searchDeployed = true
-    navSearch.classList.add('nav-search-deployed')
-
-    let backBtn = document.createElement('div');
-    backBtn.classList.add('icon');
-    backBtn.classList.add('i-left');
-    backBtn.addEventListener('click', () => {
-        backBtn.remove()
-        undeploySearch()
-    })
-    navSearchInput.insertAdjacentElement('beforebegin', backBtn)
-    navSearchInput.focus();
-    navSearchBtn.addEventListener('click', search)
-}
-
-function undeploySearch () {
-    searchDeployed = false
-    
-    navSearch.classList.remove('nav-search-deployed')
-
-    //navSearch.style.cssText = ""
-    //navSearchInput.style.cssText = ""
-}
-
 function main() {
-    navSearch = document.querySelector('.nav-search')
-    navSearchInput = document.querySelector('#nav-search-input')
-    navSearchBtn = document.querySelector('#nav-search-button')
-    navSearchBtn.addEventListener('click', search)
-    window.onresize = () => { if (searchDeployed && window.innerWidth > 480) { undeploySearch() } }
+    const navSearch = document.querySelector('.nav-search');
+    const navSearchInput = document.querySelector('#nav-search-input');
+    const navSearchBtn = document.querySelector('#nav-search-button');
+    const backBtn = navSearch.querySelector('.back-btn');
+  
+    let searchDeployed = false;
+  
+    const deploySearch = () => {
+        searchDeployed = true;
+        navSearch.classList.add('nav-search-deployed');
+        backBtn.classList.remove('hidden');
+        navSearchInput.focus();
+    };
+
+    const undeploySearch = () => {
+        searchDeployed = false;
+        navSearch.classList.remove('nav-search-deployed');
+        backBtn.classList.add('hidden');
+    };
+
+    navSearchBtn.addEventListener('click', () => {
+        if (!searchDeployed && window.innerWidth < 480) {
+            deploySearch();
+        } else {
+            console.log('googlisez', navSearchInput.value);
+        }
+    });
+
+    backBtn.addEventListener('click', undeploySearch);
+
+    window.addEventListener('resize', () => {
+        if (searchDeployed && window.innerWidth > 480) {
+            undeploySearch();
+        }
+    });
 }
 
-window.onload = main
+window.onload = main;

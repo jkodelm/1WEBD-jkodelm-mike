5b1a8a2eeb3d646c127e5cbb9d10c3008a3a829f
jklm <kylianjlm@gmail.com>
2025-01-23 20:13:26 +0100

    Cleaned tree and some pages

--- js/card-carousel.js
+++ js/card-carousel.js
@@ -76,35 +76,25 @@ class cardCarousel {
   }
 }
 
-// Fonction pour créer une carte de film avec poster, titre et résumé
 async function createMovieCard(title) {
   console.log(title)
   const card = document.createElement('div');
-  card.classList.add('temp-card');
 
-  // Ajouter un état de chargement
-  card.innerHTML = `
-      <div class="loading">Chargement...</div>
-  `;
-
-  // Récupérer les données du film depuis l'API
   const movieData = {"Title":"Cars","Year":"2006","Rated":"G","Released":"09 Jun 2006","Runtime":"116 min","Genre":"Animation, Adventure, Comedy","Director":"John Lasseter, Joe Ranft","Writer":"John Lasseter, Joe Ranft, Jorgen Klubien","Actors":"Owen Wilson, Bonnie Hunt, Paul Newman","Plot":"On the way to the biggest race of his life, a hotshot rookie race car gets stranded in a rundown town and learns that winning isn't everything in life.","Language":"English, Italian, Japanese, Yiddish","Country":"United States","Awards":"Nominated for 2 Oscars. 28 wins & 34 nominations total","Poster":"https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg","Ratings":[{"Source":"Internet Movie Database","Value":"7.3/10"},{"Source":"Rotten Tomatoes","Value":"74%"},{"Source":"Metacritic","Value":"73/100"}],"Metascore":"73","imdbRating":"7.3","imdbVotes":"490,857","imdbID":"tt0317219","Type":"movie","DVD":"N/A","BoxOffice":"$244,082,982","Production":"N/A","Website":"N/A","Response":"True"}
   // await fetchMovieData(title);
 
-  if (movieData) {
-      card.innerHTML = `
-            <div class="card">
-              <img src="${movieData.Poster}" alt="${movieData.Title}" class="movie-poster">
-              <div class="card-info">
-                  <h3>${movieData.Title}</h3>
-                  <p>${movieData.Plot || 'Aucun résumé disponible.'}</p>
-              </div>
-              <button class='button-prim'>Découvrir</button>
+  if (!movieData) { return null; }
+
+  card.innerHTML = `
+        <div class="card">
+          <img src="${movieData.Poster}" alt="${movieData.Title}" class="movie-poster">
+          <div class="card-info">
+              <h3>${movieData.Title}</h3>
+              <p>${movieData.Plot || 'Aucun résumé disponible.'}</p>
           </div>
-      `;
-  } else {
-      return null;
-  }
+          <button class='button-prim'>Découvrir</button>
+      </div>
+  `;
 
   return card;
 }
@@ -114,6 +104,7 @@ async function setupCardCarousel(carouselName, cardName, movies, gap, controlBut
   const parent = document.getElementById(carouselName);
   parent.innerHTML = '';
 
+  console.log('asousl',movies)
   for (const title of movies) {
     const card = await createMovieCard(title);
     parent.appendChild(card);
